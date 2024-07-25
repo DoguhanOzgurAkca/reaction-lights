fcb44741e7def117a8beb07d72bee4a21b124bdf
Doguhan Ozgur Akca <doguhanozgurakca@gmail.com>
2024-07-25 12:41:57 +0300

    og tags 2

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,6 @@ function App() {
   return (
     <div className="App">
       <ReactionLights />
-      <Meta
-        title="Home Page Title"
-        description="This is the description for the home page."
-        url="https://www.example.com"
-        image="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/F1_chequered_flag.svg/1024px-F1_chequered_flag.svg.png"
-      />
     </div>
   );
 }
